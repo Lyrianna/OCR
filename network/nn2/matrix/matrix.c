9e25a6b945ef4fa7ae06d38f4c0c0d61f87a868d
Lyrianna <lambrinephelie@hotmail.fr>
2019-11-17 10:15:14 +0100

    add nn2 start matrix

--- network/nn2/matrix/matrix.c
+++ network/nn2/matrix/matrix.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <err.h>
 
 #include "matrix.h"
 
@@ -18,50 +19,74 @@ void printM(Matrix mat) {
     }
 }
 
-void saveM(FILE out, Matrix mat)
+void saveM(Matrix mat)
 {
-    //TODO
-}
+    FILE* fichier = NULL;
+    fichier = fopen("datasaved.txt", "w");
 
-void freeM(Matrix mat)
-{
-    //TODO
-}
-
-void initM(Matrix *mat, size_t n, size_t p)
-{
-    mat->n = n;
-    mat->p = p;
-    mat->size = sizeof(mat);
-
-    for(size_t i = 0; i < n;i++)
+    if (fichier != NULL)
     {
-        for(size_t j = 0; j < p;j++)
+        for (int i = 0; i < mat.n; i++)
         {
-            mat->matrix[i*n+j] = 0;
+            for (int j = 0; j < mat.p; ++j) {
+                fprintf(fichier,"%lf ",mat.matrix[i*mat.n+j]);
+            }
         }
+
+        fclose(fichier);
     }
 }
 
-void addM(Matrix mat1, Matrix mat2, Matrix *result)
+void freeM(Matrix* mat)
 {
+    free(mat->matrix);
+    free(mat);
+}
+
+Matrix* initM(size_t n, size_t p)
+{
+    size_t size = n*p;
+
+    Matrix* init = malloc(sizeof(Matrix));
+
+    init->n = n;
+    init->p = p;
+    init->sizevector = size;
+    init->matrix = calloc(size, sizeof(double));
+
+    return init;
+}
+
+Matrix* addM(Matrix mat1, Matrix mat2)
+{
+    if(mat1.n!= mat2.n || mat1.p != mat2.p)
+    {
+        errx(1, "ADD : matrices do not have the same dimension. M1(%i,%i) and M2(%i,%i).\n",
+                mat1.n,mat1.p,mat2.n,mat2.p);
+    }
+
     size_t rows = mat1.n;
     size_t cols = mat2.p;
+
+    Matrix* result = initM(rows,cols);
 
     for (size_t i = 0; i < rows; i++)
     {
         for (size_t j = 0; i < cols;i++)
         {
-            result->matrix[i*cols+j] = (mat1.matrix[i*cols+j]) + (mat2.matrix[i*cols+j]);
+            result->matrix[i*cols+j] = ((mat1.matrix[i*cols+j]) + (mat2.matrix[i*cols+j]));
         }
     }
+    return result;
 }
 
-void mulM(Matrix mat1, Matrix mat2, Matrix *result)
+Matrix* mulM(Matrix mat1, Matrix mat2)
 {
     size_t r1 = mat1.n;
     size_t c1 = mat1.p;
     size_t c2 = mat2.p;
+
+    Matrix* result = initM(//TODO);
 
     for (size_t i = 0; i<r1; i++)
     {
@@ -75,10 +100,12 @@ void mulM(Matrix mat1, Matrix mat2, Matrix *result)
     }
 }
 
-void hadaM(Matrix mat1, Matrix mat2, Matrix *result)
+Matrix* hadaM(Matrix mat1, Matrix mat2)
 {
     size_t rows = mat1.n;
     size_t cols = mat2.p;
+
+    Matrix* result = initM(//TODO);
 
     for (size_t i = 0; i < rows; i++)
     {
@@ -89,138 +116,47 @@ void hadaM(Matrix mat1, Matrix mat2, Matrix *result)
     }
 }
 
-void scalM(Matrix *result, Matrix m, double s)
-{
-    //TODO
-}
-
-void dotM(Matrix *result, Matrix a, Matrix b)
-{
-    //TODO
-}
-
-void transpM(Matrix *result, Matrix m)
+Matrix* scalM(Matrix m, double s)
 {
     size_t rows = m.n;
     size_t cols = m.p;
+
+    Matrix* result = initM(//TODO);
+    //TODO
+}
+
+Matrix* dotM(Matrix a, Matrix b)
+{
+    /*size_t rows = m.n;
+    size_t cols = m.p;*/
+
+    Matrix* result = initM(//TODO);
+    //TODO
+}
+
+Matrix* transpM(Matrix *m)
+{
+    size_t rows = m->n;
+    size_t cols = m->p;
+
+    Matrix* result = initM(//TODO);
 
     for (size_t i=0; i<rows; i++)
     {
         for(size_t j=0; j<cols; j++)
         {
-            result->matrix[j*rows+i]=m.matrix[i*cols+j];
+            result->matrix[j*rows+i] = m->matrix[i*cols+j];
         }
     }
 }
 
-void sigM(Matrix *result, Matrix m)
+Matrix* sigM(Matrix m)
 {
+    //TODO errx
+
+    size_t rows = m.n;
+    size_t cols = m.p;
+
+    Matrix* result = initM(rows,cols);
     //TODO
 }
-
-//MAIN
-
-
-#define ROWS1 1
-#define COLS1 2
-
-Matrix m1;
-
-initM(m1,1,2);
-
-/*double m1[] =
-        {
-                1, 2
-        };*/
-
-#define ROWS2 2
-#define COLS2 2
-double m2[] =
-        {
-                1, 2,
-                3, 4
-        };
-
-#define ROWS3 3
-#define COLS3 2
-double m3[] =
-        {
-                1, 2,
-                3, 4,
-                5, 6
-        };
-
-#define ROWS4 4
-#define COLS4 3
-double m4[] =
-        {
-                1, 0, 3,
-                1, 2, 0,
-                0, 2, 3,
-                1, 0, 3
-        };
-
-#define ROWS5 3
-#define COLS5 4
-double m5[] =
-        {
-                1, 2, 3, 4,
-                5, 6, 7, 8,
-                9, 10, 11, 12,
-        };
-
-// Results for the transpositions.
-double m1_tr[ROWS1 * COLS1];
-double m2_tr[ROWS2 * COLS2];
-double m3_tr[ROWS3 * COLS3];
-double m4_tr[ROWS4 * COLS4];
-double m5_tr[ROWS5 * COLS5];
-
-// Results for the additions.
-double m3_plus_m3[ROWS3 * COLS3];
-double m4_plus_m5tr[ROWS4 * COLS4];
-double m5_plus_m4tr[ROWS5 * COLS5];
-
-// Results for the multiplications.
-double m2_times_m2[ROWS2 * COLS2];
-double m4_times_m5[ROWS4 * COLS5];
-double m5_times_m4[ROWS5 * COLS4];
-
-int main()
-{
-    printM(m1);
-    print_matrix("m2", m2, ROWS2, COLS2);
-    print_matrix("m3", m3, ROWS3, COLS3);
-    print_matrix("m4", m4, ROWS4, COLS4);
-    print_matrix("m5", m5, ROWS5, COLS5);
-
-    transpose(m1,ROWS1,COLS1,m1_tr);
-    print_matrix("m1_tr",m1_tr,COLS1,ROWS1);
-    transpose(m2,ROWS2,COLS2,m2_tr);
-    print_matrix("m2_tr",m2_tr,COLS2,ROWS2);
-    transpose(m3,ROWS3,COLS3,m3_tr);
-    print_matrix("m3_tr",m3_tr,COLS3,ROWS3);
-    transpose(m4,ROWS4,COLS4,m4_tr);
-    print_matrix("m4_tr",m4_tr,COLS4,ROWS4);
-    transpose(m5,ROWS5,COLS5,m5_tr);
-    print_matrix("m5_tr",m5_tr,COLS5,ROWS5);
-
-    add(m3,m3,ROWS3,COLS3,m3_plus_m3);
-    print_matrix("m3_plus_m3",m3_plus_m3,ROWS3,COLS3);
-    add(m4,m5_tr,ROWS4,COLS4,m4_plus_m5tr);
-    print_matrix("m4_plus_m5tr",m4_plus_m5tr,ROWS4,COLS4);
-    add(m5,m4_tr,ROWS5,COLS5,m5_plus_m4tr);
-    print_matrix("m5_plus_m4tr",m5_plus_m4tr,ROWS5,COLS5);
-
-    mul(m2,m2,ROWS2,COLS2,COLS2,m2_times_m2);
-    print_matrix("m2_times_m2",m2_times_m2,ROWS2,COLS2);
-    mul(m4,m5,ROWS4,COLS4,COLS5,m4_times_m5);
-    print_matrix("m4_times_m5",m4_times_m5,ROWS4,COLS5);
-    mul(m5,m4,ROWS5,COLS5,COLS4,m5_times_m4);
-    print_matrix("m5_times_m4",m5_times_m4,ROWS5,COLS4);
-
-    return 0;
-}
-
-
-
--- network/nn2/matrix/matrix.h
+++ network/nn2/matrix/matrix.h
@@ -9,23 +9,23 @@ typedef struct Matrix{
 
     size_t n;
     size_t p;
-    size_t size;
-    double matrix[];
+    size_t sizevector;
+    double *matrix;
 } Matrix ;
 
 
 void printM(Matrix mat);
-void saveM(FILE out, Matrix mat);
-void freeM(Matrix mat);
+void saveM(Matrix mat);
+void freeM(Matrix* mat);
 
-void initM(Matrix *mat, size_t n, size_t p);
+Matrix* initM(size_t n, size_t p);
 
-void addM(Matrix mat1, Matrix mat2, Matrix *result);
-void mulM(Matrix mat1, Matrix mat2, Matrix *result);
-void hadaM(Matrix mat1, Matrix mat2, Matrix *result);
-void scalM(Matrix *result, Matrix m, double s);
-void dotM(Matrix *result, Matrix a, Matrix b);
-void transpM(Matrix *result, Matrix m);
-void sigM(Matrix *result, Matrix m);
+Matrix* addM(Matrix mat1, Matrix mat2);
+Matrix* mulM(Matrix mat1, Matrix mat2);
+Matrix* hadaM(Matrix mat1, Matrix mat2);
+Matrix* scalM(Matrix m, double s);
+Matrix* dotM(Matrix a, Matrix b);
+Matrix* transpM(Matrix *m);
+Matrix* sigM(Matrix m);
 
 #endif //NN2_MATRIX_H
