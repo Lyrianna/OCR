2407639c0c398183b927747c31a1a7faa125ee0f
Lyrianna <lambrinephelie@hotmail.fr>
2019-11-30 16:01:43 +0100

    train presque fonctionnel

--- network/nn2/main.c
+++ network/nn2/main.c
@@ -6,6 +6,7 @@
 #include "networkV2.h"
 #include "matrix/matrix.h"
 //#include <math.h>
+#include <string.h>
 
 int main(int argc, char** argv)
 {
@@ -27,18 +28,17 @@ int main(int argc, char** argv)
         errx(1,"MAIN : No matrice is specified.");
     else
     {
-        int arg = (int) atoi(argv[1]);
-
+        char* arg = argv[1];
+        printf("%s",argv[1]);
         bool istherearg = (argc==3) ? true : false ;
 
-        switch (arg) {
-            case 'train':
-                train_neural(xor,wanted_output,istherearg,(int) atoi(argv[2]));
-            default:
-                printf("echec\n");
-                return 1;
-                //character_translator(in,filename,istherearg,argv[2]);
+        if (strcmp(arg, "train") == 0)
+        {
+            train_neural(xor,wanted_output,istherearg,(int) atoi(argv[2]));
         }
+
+        /*else
+                character_translator(in,filename,istherearg,argv[2]);*/
     }
 
     return 0;
