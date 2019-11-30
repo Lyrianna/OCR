2483d768dd0a3d650c45c5b5d7eaa7a2ad4a6bb6
Sarah Gutierez <sarah.gutierez@epita.fr>
2019-11-30 18:29:47 +0100

    softmax

---
+++ networkexamples/test softmax/final_neural_network/matrix/matrix.h
@@ -0,0 +1,38 @@
+//
+// Created by Néphélie on 13/11/2019.
+//
+#ifndef NN2_MATRIX_H
+#define NN2_MATRIX_H
+
+#include <stdbool.h>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <err.h>
+
+typedef struct Matrix{
+
+    size_t n;
+    size_t p;
+    size_t sizevector;
+    double *matrix;
+} Matrix ;
+
+void printM(Matrix* mat, char s[]);
+void saveM(Matrix *mat, bool iscontinuous);
+void freeM(Matrix* mat);
+
+Matrix* initM(size_t n, size_t p);
+void initM2(Matrix *mat, size_t n, size_t p);
+
+Matrix* addM(Matrix* mat1, Matrix* mat2);
+Matrix* subM(Matrix* mat1, Matrix* mat2);
+Matrix* mulM(Matrix* mat1, Matrix* mat2);
+Matrix* dotM(Matrix* mat1, Matrix* mat2);
+Matrix* scalM(Matrix* m, double s);
+Matrix* transpM(Matrix* m);
+Matrix* sigM(Matrix* m, bool is_derivate);
+Matrix* softmaxM(Matrix* m);
+Matrix* initwithvaluesM(size_t n, size_t p, double *m);
+
+#endif //NN2_MATRIX_H
---
+++ networkexamples/test softmax/final_neural_network/networkV2.h
@@ -0,0 +1,41 @@
+#ifndef XOR_NETWORK_H
+#define XOR_NETWORK_H
+
+#include "matrix/matrix.h"
+
+
+
+//initialization
+void generate_wgt();
+void initAll();
+
+//Feed forward
+void hidden_layers();
+void output_neurons();
+
+//Backpropagation
+void error();
+void derivatives();
+
+//Update of weights and bias
+void update_weights();
+
+//training
+void train_neural(Matrix *in , Matrix *wanted_out);
+
+//character finder
+void character_translator(Matrix* in, char* filename);
+
+//Displays the results
+void displayepoch();
+
+//Save datas
+void save_datas();
+
+//loadmatric
+void load_datas(Matrix* matrixarray[], FILE* fichier);
+
+//free the memory
+void freeAll();
+
+#endif
